ff72533487aa359a0ac053a1e7c136aa8a7990e9
beckman924 <braiancarabajal@gmail.com>
2024-07-05 16:27:08 -0300

    Added tests config and basic test

---
+++ __tests__/app/Home.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+
+import MockIntersectionObserver from "../../__mocks__/intersectionObserverMock";
+import MainWrapper from "@components/Main/MainWrapper";
+
+describe("MainWrapper", () => {
+  it("renders About, Interests, and Contact sections", () => {
+    window.IntersectionObserver = MockIntersectionObserver;
+    render(<MainWrapper />);
+    expect(
+      screen.getByRole("heading", { level: 2, name: /Sobre mí/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { level: 2, name: /Intereses/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { level: 2, name: /Contacto/i })
+    ).toBeInTheDocument();
+  });
+});
